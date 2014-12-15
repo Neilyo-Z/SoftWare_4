72bd18b374149cd4146f877fc2cb53245ead3e37
BlodSide <270100210@qq.com>
2014-12-16 02:05:44 +0800

    完成了欢迎界面的框架 事件处理还没加

---
+++ src/client/src/client/Client.java
@@ -0,0 +1,8 @@
+package client;
+
+class Client{
+	public static void main(String[] args){
+		WelcomeWindow welcomeWindow = new WelcomeWindow();
+		welcomeWindow.main(null);
+	}
+}
--- src/client/src/client/WelcomeWindow.java
+++ src/client/src/client/WelcomeWindow.java
@@ -44,6 +44,7 @@ public class WelcomeWindow extends JFrame {
 				}
 			}
 		});
+		
 	}
 
 	/**
