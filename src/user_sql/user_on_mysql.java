16fc707ec839501192beef5195d8a92aff49be10
Neilyo-Z <121220319@smail.nju.edu.cn>
2014-12-16 01:17:24 +0800

    添加login的数据库接口 Signed-off-by: Neilyo-Z <121220319@smail.nju.edu.cn>

--- src/user_sql/user_on_mysql.java
+++ src/user_sql/user_on_mysql.java
@@ -11,6 +11,8 @@ public class user_on_mysql  {
 	static String url="jdbc:mysql://localhost:3306/user_db";
 	static String username="root";
 	static Statement statement;
+	static String status_off="offline";
+	static String status_on="online";
 	//初始化与数据库的连接
 	public static void init() throws ClassNotFoundException{
 		try {
@@ -32,5 +34,22 @@ public class user_on_mysql  {
 	public static void close() throws SQLException{
 		con.close();
 	}
-	
+	public static boolean Login(String user_id,String password) throws SQLException{
+		synchronized (statement) {
+			//选出处于离线状态的符合用户名密码的用户
+			String query="select * from user_info where user_id='"+user_id
+					+"' and password='"+password+"' and status='"+status_off+"';";
+			ResultSet rs=statement.executeQuery(query);
+			//若不存在符合条件的用户则登录失败
+			if(!rs.next())
+				return false;
+			else {
+				//更新该用户的status为status_on
+				String update_query="update user_info set status='"+status_on+
+						"' where user_id='"+user_id+"' and password='"+password+"';";
+				statement.executeUpdate(update_query);
+				return true;
+			}
+		}
+	}
 }
